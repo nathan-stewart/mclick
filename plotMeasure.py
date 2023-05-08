669a799e63e3420229f7ef24a4fd8fb57665a461
Nathan Stewart <nathan-stewart@users.noreply.github.com>
2023-05-07 23:26:42 -0400

    rewriting manually

--- plotMeasure.py
+++ plotMeasure.py
@@ -1,111 +1,118 @@
 #!/usr/bin/env python3
 import mido
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from collections import defaultdict
+
+
+def build_plottables(events):
+    """
+    Builds plottable objects for MIDI events.
+    """
+    # Collect note numbers for all events
+    note_numbers = sorted(list(set(n for ch_events in events.values() for n, *_ in ch_events)))
+
+    # Create a list of channel names for the plot legend
+    channel_names = [f"Ch {ch}" for ch in sorted(events.keys())]
+
+    # Create a 2D array of note durations for each channel and note
+    note_durations = np.zeros((len(channel_names), len(note_numbers)))
+    time_values = None  # Placeholder for time values
+
+    for i, ch in enumerate(sorted(events.keys())):
+        try:
+            for note, start_time, duration in events[ch]:
+                if time_values is None:
+                    time_values = np.zeros(len(events[ch]))
+
+                if duration == 0:
+                    # Assign a default duration value for notes without a corresponding note-off event
+                    duration = 1
+                try:
+                    note_durations[i, note_numbers.index(note)] = duration
+                    time_values[note_numbers.index(note)] = start_time
+                except ValueError:
+                    continue
+        except ValueError:
+            continue
+    return time_values, note_durations, channel_names
+
 
 def plot_midi_events(events):
     """
     Plots MIDI events organized by channel and note number.
     """
-    # Collect note numbers for all events
-    all_notes = sorted(list(set(n for ch_events in events.values() for n, _, _ in ch_events)))
-    
-    # Create a list of channel names for the plot legend
-    channel_names = [f"Ch {ch}" for ch in sorted(events.keys())]
-    
-    # Create a 2D array of note durations for each channel and note
-    note_durations = np.zeros((len(channel_names), len(all_notes)))
-    for i, ch in enumerate(sorted(events.keys())):
-        for note, start_time, duration in events[ch]:
-            note_durations[i, all_notes.index(note)] = duration
-    
+    print_events(events)
+    import sys
+    sys.exit(0)
+    # Build the plottable objects
+    time_values, note_durations, channel_names = build_plottables(events)
+
     # Plot the note durations using a stacked bar plot
     fig, ax = plt.subplots()
-    ax.bar(np.arange(len(all_notes)), note_durations[0], align='center')
+    ax.bar(time_values, note_durations[0], align='center')
+
     for i in range(1, len(channel_names)):
-        ax.bar(np.arange(len(all_notes)), note_durations[i], bottom=np.sum(note_durations[:i], axis=0), align='center')
-    
+        ax.bar(
+            time_values,
+            note_durations[i],
+            bottom=np.sum(note_durations[:i], axis=0),
+            align='center'
+        )
+
     # Format the plot
-    ax.set_xticks(np.arange(len(all_notes)))
-    ax.set_xticklabels(all_notes)
-    ax.set_xlabel("Note Number")
-    ax.set_ylabel("Time (ticks)")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Duration")
     ax.legend(channel_names, loc='upper right')
     plt.show()
 
-def plot_midi_file(mid):
+def print_events(events):
+    for channel in sorted(events.keys()):
+        print('channel = %d' % channel)
+    import sys
+    sys.exit(0)
+
+def parse_midi_file(mid):
     """
-    Parse the contents of a MIDI file into a dictionary of events.
+    Parse the contents of a mido.MidiFile into a dictionary of events.
 
     Returns:
-        A dictionary where each key is a MIDI channel number (0-15), and the value is a list of tuples
-        representing the note events on that channel. Each tuple contains (note number, tick time since last event).
+        A dictionary where each key is a MIDI channel, and the value is a nested dictionary.
+        All tracks processed, but it is assumed that one track = one channel and vice versa
+        The nested dictionary has note numbers as keys, and the values are lists of timestamp
+        tuples (start_ticks, stop_ticks) in ticks where stop_ticks is None if no note off occured
     """
-    events = defaultdict(list)
-    note_start_times = {}
-    
+    events = defaultdict(lambda: defaultdict(list))
+    cumulative_ticks = 0
+
+    # Takes a list of note on/off events for a single channel/note number
+    # Returns the oldest one without a stop event [start,stop]
+    def oldest_ringing(notes): 
+        return next((n for n in notes if n[1]), None)
+
+    def silence(event):
+        if event:
+            start = event[0]
+            event.clear()
+            event = (start, cumulative_ticks)
+
     for i, track in enumerate(mid.tracks):
+        cumulative_ticks = 0
         for msg in track:
             cumulative_ticks = 0
             if not msg.is_meta:
-                event = events.get(msg.channel)
-                if event:
-                    cumulative_ticks = event[0][1]
-                    for channel in events:
-                        events[channel].insert(0, (events[channel][0][0], events[channel][0][1] - cumulative_ticks))
-                if msg.type == 'note_on':
-                    if msg.velocity != 0:
-                        if msg.channel not in events:
-                            events[msg.channel] = []
-                        if msg.note not in note_start_times:
-                            note_start_times[msg.note] = msg.time
-                        else:
-                            note_duration = msg.time - note_start_times[msg.note]
-                            events[msg.channel].append((msg.note, note_start_times[msg.note], note_duration))
-                            del note_start_times[msg.note]
-                elif msg.type == 'note_off':
-                    if msg.channel in events:
-                        for i, (note, tick, _) in enumerate(reversed(events[msg.channel])):
-                            if note == msg.note:
-                                events[msg.channel][-i - 1] = (note, tick, msg.time)
-                                break
-                else:
-                    for channel in events:
-                        events[channel][0] = (events[channel][0][0], events[channel][0][1] + msg.time)
-                        
-        # End any notes that were started on this track but not explicitly ended
-        for note, start_time in note_start_times.items():
-            if not msg.is_meta:
-                events[msg.channel].append((note, start_time, mid.length - start_time))
-                del note_start_times[note]
-        
-        # Reverse each track so that the notes are in chronological order
-        for channel in events:
-            events[channel] = list(reversed(events[channel]))
-    return events 
-
-
-def parse_midi_file(file_path):
-    midi_file = mido.MidiFile(file_path)
-    events = defaultdict(list)
-    for i, track in enumerate(midi_file.events):
-        cumulative_ticks = 0
-        for msg in track:
-            cumulative_ticks += msg.time
-            if msg.type == 'note_on':
-                events[msg.channel].append((msg.note, cumulative_ticks))
-            elif msg.type == 'note_off':
-                for i, (note, tick) in enumerate(reversed(events[msg.channel])):
-                    if note == msg.note:
-                        duration = cumulative_ticks - tick
-                        events[msg.channel][-1-i] = (note, tick, duration)
-                        break
-                else:
-                    print(f"Warning: Note off with no corresponding note on. Channel: {msg.channel}, Note: {msg.note}")
+                cumulative_ticks += msg.time
+                if msg.type == 'note_on' or msg.type == 'note_off':
+                    notes = events[msg.channel][msg.note]
+                    if (msg.type == 'note_on' and msg.velocity > 0): # note on
+                        notes.append( [cumulative_ticks, None] )
+                    else: 
+                        silence(oldest_ringing(notes))
     return events
+
+def plot_midi_file(m):
+    plot_midi_events(parse_midi_file(m))
 
 if __name__ == '__main__':
     f = mido.MidiFile('demo/cwm_rhondda.mid')
     plot_midi_file(f)
-
