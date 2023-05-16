ad6716304f6c98ae1c489c21298a81caeb490d40
Nathan Stewart <nathan-stewart@users.noreply.github.com>
2023-05-15 22:31:05 -0400

    threw away our merge and used mido track merge

--- buildMeasure.py
+++ buildMeasure.py
@@ -3,27 +3,19 @@ import mido
 from sortedcontainers import SortedList
 from settings import settings
 
-class AbstimeMessage:
-    def __init__(self, t, m):
-        self.t = t
-        self.m = m
-
-    def __lt__(self, other):
-        return self.t < other.t
-
 def make_template_measure(params, ppq=480):
-    rhythmq = SortedList()
-    eight_track = SortedList()
+    rhythm = []
 
-    def enqueue(que, partname, events):
+    def add_track(partname, events):
+
         last_t = 0
-        last_t = 0
+        track = mido.MidiTrack()
         for t in events:
             msg = None
             if partname == 'clock':
                 msg = mido.Message('clock',time=t-last_t)
             else:
-                v  = params[partname]['volume']
+                v = params[partname]['volume']
                 n = params[partname]['note']
                 msg = mido.Message('note_on',
                                     channel=9,
@@ -31,74 +23,86 @@ def make_template_measure(params, ppq=480):
                                     velocity=v,
                                     time=t-last_t)
             if msg:
-                que.add(AbstimeMessage(t,msg))
+                track.append(msg)
             last_t = t
+        return track
 
-    beat_count = params['num_beats']
-    clock = [int(n * ppq / 24.0) for n in range(24 * beat_count)]
-    enqueue(rhythmq,'clock', clock)
-    enqueue(rhythmq,'measure', [0])
+    ts = params['time_signature']
+    tpb = int(ppq * 4 / ts[1])
+    clock = [int(n * tpb / 24.0) for n in range(24 * ts[0])]
+    rhythm.append(add_track('clock', clock))
+    rhythm.append(add_track('measure', [0]))
+    #print('measure (%d) = ' % (params['measure']['note']), [0])
 
     skip = False
     if 'skip' in params['beat'].keys():
         skip = params['beat']['skip']
     swing = min(1.0, max(0.0, params['swing'] / 100.0))
-    compound = True if beat_count in [6,9,12] else False
+    compound = True if ts[0] in [6,9,12] else False
 
     # don't play the one - it's played by the measure
-    beats = [int(b * ppq)  for b in range(1,beat_count) ]
-    if skip and beat_count == 3: # in 3/4 time, skip beat plays the 3
-        beats = [2 * ppq]
-    elif skip and  beat_count == 4: # in 4/4 time skip beat plays 2 and 4
-        beats = [b * ppq for b in [1,3]]
+    beats = [int(b * tpb)  for b in range(1,ts[0]) ]
+    if skip and ts[0] == 3: # in 3/4 time, skip beat plays the 3
+        beats = [2 * tpb]
+    elif skip and ts[0] == 4: # in 4/4 time skip beat plays 2 and 4
+        beats = [b * tpb for b in [1,3]]
     elif compound:
         # signatures considered compound
         # we play beats on 1, 4, 7, 10 and 8ths elsewhere
-        beats = [int(b * ppq) for b in range(1,beat_count) if not b%3]
-    enqueue(rhythmq, 'beat', beats)
+        beats = [int(b * tpb) for b in range(1,ts[0]) if not b%3]
+    #print('beat(%d) = ' % (params['beat']['note']), beats)
+    rhythm.append(add_track('beat', beats))
 
     if compound:
         # compounds play eights as weak beats - they're not half beats
         # we skipped these beats above
-        eighths = [ int(ppq * n) for n in range(beat_count) if n%3 ]
+        eighths = [ int(n * tpb) for n in range(ts[0]) if n % 3 ]
     else:
-        eights = [ int(ppq * n /2.0) for n in range(beat_count * 2)]
+        eights = [ int(n * tpb  /2.0) for n in range(ts[0]* 2)]
         # capping swing at dotted eighth as a hard swing
-        sw_adj = int(ppq / 4.0 * swing)
-        eighths = [ int(ppq * n/2.0 +(n%2)*sw_adj) for n in range(beat_count * 2) ]
-    enqueue(eight_track, 'eighths', eighths)
+        sw_adj = int(tpb * swing / 4.0)
+        eighths = [ int(n * tpb /2.0 +(n % 2) * sw_adj) for n in range(ts[0] * 2) ]
+    #print('eighths(%d) = ' % (params['eighths']['note']), eighths)
+    rhythm.append(add_track('eighths', eighths))
 
     subdivide = True if ((params['sixteenths']['volume'] > 0) and
                          (params['eighths']['volume'] > 0) and
                          not compound) else False
     if subdivide:
         # change the eight notes to the same sound as sixteenths
-        for abstime in eight_track:
-            abstime.m.note = params['sixteenths']['note']
+        for m in rhythm[-1]:
+            if hasattr(m, 'note'):
+             m.note = params['sixteenths']['note']
 
     sixteenths = []
     if compound:
-        sw_adj = int(ppq / 4.0  * swing)
-        sixteenths = [int(ppq * n /2.0 + sw_adj*(n%2)) for n in range(beat_count * 2) ]
+        sw_adj = int(tpb / 4.0  * swing)
+        sixteenths = [int(n * tpb  /2.0 + sw_adj*(n%2)) for n in range(ts[0] * 2) ]
     else:
         # if time signature is simple and we are swinging eighth's don't play 16ths
         if swing <= 0.001:
-            sixteenths = [int(ppq * n / 4.0) for n in range(beat_count * 4)]
-    enqueue(rhythmq, 'sixteenths', sixteenths)
-
-    # we kept the eighths separate until now - merge them into the priority queue
-    for e in eight_track:
-        rhythmq.add(e)
-
-    percussion = mido.MidiTrack()
-    while rhythmq:
-        percussion.append(rhythmq.pop(0).m)
-    return percussion
+            sixteenths = [int(n * ppq / 4.0) for n in range(ts[0] * 4)]
+    #print('sixteenths(%d) = ' % (params['sixteenths']['note']), sixteenths)
+    rhythm.append(add_track('sixteenths', sixteenths))
+    
+    return mido.merge_tracks(rhythm)
 
 if __name__ == "__main__":
     from plotMeasure import plot_midi, plot_midi_events
-    m = make_template_measure(settings)
-    plot_midi(m)
+    for ts in settings['measure_options']:
+        settings['time_signature'] = ts
+        settings['measure']['note'] = 10
+        settings['measure']['volume'] = 127
+        settings['beat']['note'] = 11
+        settings['beat']['volume'] = 63
+        settings['eighths']['note'] = 12
+        settings['eighths']['volume'] = 31
+        settings['sixteenths']['note']= 13
+        settings['sixteenths']['volume']= 0
+        settings['swing'] = 0
+        m = make_template_measure(settings, ppq=100)
+        plot_midi(m)
+        print(m)
     
     
 
